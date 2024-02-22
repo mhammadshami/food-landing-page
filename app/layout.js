f510b704ab832baa2017f82c571beed573afdbaf
mhammadshami <mhammadshami3333@gmail.com>
2024-02-22 16:56:59 +0200

    first commit

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,16 @@
-import { Inter } from "next/font/google";
+import { Lora, Poppins } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const lora = Lora({
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700"],
+  variable: "--font-lora",
+});
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  variable: "--font-poppins"
+});
 
 export const metadata = {
   title: "Create Next App",
@@ -11,7 +20,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`${lora.variable} ${poppins.variable}`}>{children}</body>
     </html>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,73 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: ["class"],
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './pages/**/*.{js,jsx}',
+    './components/**/*.{js,jsx}',
+    './app/**/*.{js,jsx}',
+    './src/**/*.{js,jsx}',
   ],
+  prefix: "",
   theme: {
+    container: {
+      center: true,
+      padding: "15px",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
+    screens: {
+      sm: '640px',
+      md: '768px',
+      lg: '960px',
+      xl: '1200px'
+    },
+    colors:{ 
+      body: '#e5e5e5',
+      white: '#FFF',
+      green: {
+        DEFAULT: '#539592',
+        hover: '#40807d'
+      },
+      black: {
+        DEFAULT: "#273029",
+        heavy: "#1b211c"
+      },
+      grey: "#888888",
+      orange: {
+        DEFAULT: '#f2994a',
+        hover: '#da863c',
+      },
+      outline: "#f1f1f1",
+      pink: '#ffa5a5'
+    },
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      fontFamily: {
+        lora: ['var(--font-lora)', 'sans-serif'],
+        poppins: ['var(--font-poppins)', 'sans-serif']
+      },
+      backgroundImage:{
+        
+      },
+      boxShadow: {
+        primary: '40px 4px 40px 0 rgba(68, 68, 68, 0.25)',
+
+      },
+      keyframes: {
+        "accordion-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
       },
     },
   },
-  plugins: [],
-};
+  plugins: [require("tailwindcss-animate")],
+}
